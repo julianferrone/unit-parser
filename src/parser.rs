06267229b98c1e3f4f444b12caca4793fbca7a8a
jules <81225111+jules@users.noreply.github.com>
2023-09-09 17:05:42 +1000

    Parser converts physical quantities

--- src/parser.rs
+++ src/parser.rs
@@ -12,9 +12,11 @@ use nom::{
     IResult,
 };
 
-fn nonws_char(c: char) -> bool {
-    !is_space(c as u8) && !is_newline(c as u8)
-}
+use crate::{PhysicalQuantity, PhysicalQuantityBuilder};
+
+// fn nonws_char(c: char) -> bool {
+//     !is_space(c as u8) && !is_newline(c as u8)
+// }
 
 fn alphabet_char(c: char) -> bool {
     is_alphabetic(c as u8)
@@ -32,18 +34,31 @@ fn parse_isize(input: &str) -> IResult<&str, isize> {
     Ok((i, number))
 }
 
-pub fn unit(input: &str) -> IResult<&str, (&str, isize)> {
+pub fn unit_as_tuples(input: &str) -> IResult<&str, (&str, isize)> {
     alt((
         separated_pair(word, char('^'), parse_isize),
         map(word, |s: &str| (s, 1isize)),
     ))(input)
 }
 
-fn units(input: &str) -> IResult<&str, Vec<(&str, isize)>> {
-    separated_list0(char(' '), unit)(input)
+fn units_as_physical_quantities(input: &str) -> IResult<&str, PhysicalQuantity> {
+    map(unit_as_tuples, |(s, i)| match s {
+        "s" => PhysicalQuantityBuilder::new().time(i).build(),
+        "m" => PhysicalQuantityBuilder::new().length(i).build(),
+        "kg" => PhysicalQuantityBuilder::new().mass(i).build(),
+        "A" => PhysicalQuantityBuilder::new().current(i).build(),
+        "K" => PhysicalQuantityBuilder::new().temperature(i).build(),
+        "mol" => PhysicalQuantityBuilder::new().amount_of_substance(i).build(),
+        "cd" => PhysicalQuantityBuilder::new().luminous_intensity(i).build(),
+        _ => PhysicalQuantityBuilder::new().build(),
+    })(input)
 }
 
-pub fn concrete_number(input: &str) -> IResult<&str, (f64, Vec<(&str, isize)>)> {
+fn units(input: &str) -> IResult<&str, Vec<PhysicalQuantity>> {
+    separated_list0(char(' '), units_as_physical_quantities)(input)
+}
+
+pub fn concrete_number(input: &str) -> IResult<&str, (f64, Vec<PhysicalQuantity>)> {
     separated_pair(double, many1(char(' ')), units)(input)
 }
 
